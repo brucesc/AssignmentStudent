b94a9f253d3a0881bf15d544257e34e7275244ce
Steven Bruce <Brucesc726@gmail.com>
2018-02-23 09:45:47 -0500

    Initialization

---
+++ AssignmentStudent/Student.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace AssignmentStudent
+{
+    public class Student
+    {
+        public Student(string name, string major, string college, int gradyear, bool gradwithhonors)
+        {
+            Name = name;
+            Major = major;
+            College = college;
+            GradYear = gradyear;
+            GradWithHonors = gradwithhonors;
+
+        }
+        public Student(string name, string major, string college, int gradyear, bool gradwithhonors, bool alumnidoner)
+        {
+            Name = name;
+            Major = major;
+            College = college;
+            GradYear = gradyear;
+            GradWithHonors = gradwithhonors;
+            AlumniDoner = alumnidoner;
+        }
+
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string Major { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string College { get; set; }
+        public int GradYear { get; set; }
+        public bool GradWithHonors { get; set; }
+        public bool? AlumniDoner { get; set; }
+    }
+}
---
+++ AssignmentStudent/Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace AssignmentStudent
+{
+    public class Test
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string TestName { get; set; }
+        public decimal Score { get; set; }
+        public int StudentId { get; set; }
+
+        public virtual Student Student { get; set; }
+    }
+}
